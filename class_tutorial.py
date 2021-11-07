c7fe7ed4bddc9352865636247520c005e7a4eefe
alam51 <jccbuet1@gmail.com>
2021-11-07 10:44:10 +0600

    class_tutorial_product.py added

--- class_tutorial.py
+++ class_tutorial.py
@@ -3,7 +3,18 @@ class Point:
         self.x = x
         self.y = y
 
+    def __str__(self):
+        return f"(x,y)=({self.x},{self.y})"
+
+    @classmethod
+    def zero(cls):
+        return cls(0, 0)
+
     def describe(self):
         print(f'x={self.x}, y={self.y}')
 
 
+p1 = Point(1, 2)
+print(p1)
+p0 = Point.zero()
+print(p0)
